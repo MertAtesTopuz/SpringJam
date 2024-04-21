b7c9e341e801cbd5f79042660ffe537d82235aa5
BugKillR <148430215+BugKillR@users.noreply.github.com>
2024-04-21 11:05:54 +0300

    Commit 26
    Town Level Design

--- Assets/Scripts/Godzillah.cs
+++ Assets/Scripts/Godzillah.cs
@@ -5,20 +5,36 @@ using UnityEngine.InputSystem;
 
 public class Godzillah : MonoBehaviour
 {
-    public GameObject cam;
+    //public GameObject cam;
+
+    #region Movement#
+    private PlayerInput playerInput;
+    private InputAction moveAction;
 
     [SerializeField] float movementSpeed;
     [SerializeField] float rotationSpeed;
     [SerializeField] float mesafeX;
     [SerializeField] float mesafeY;
 
+    private bool isCarrying;
+    #endregion#
+
+
+    #region Rotation#
+
+
+    #endregion#
+
+
     private Animator anim;
     private Rigidbody rb;
     private CapsuleCollider coll;
 
+    private Vector2 dir;
+
     private void Awake()
     {
-       // anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
         coll = GetComponent<CapsuleCollider>();
     }
@@ -26,28 +42,38 @@ public class Godzillah : MonoBehaviour
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        playerInput = GetComponent<PlayerInput>();
+        moveAction = playerInput.actions.FindAction("Move");
     }
 
     void Update()
-    {;
+    {
+        dir = MovePlayer();
+        RotatePlayer();
+
         Animation();
 
-        cam.transform.position = new Vector3(transform.position.x - mesafeX, cam.transform.position.y ,cam.transform.position.y - mesafeY);
+        //cam.transform.position = new Vector3(transform.position.x - mesafeX, cam.transform.position.y, cam.transform.position.y - mesafeY);
     }
 
-    public void MovePlayer(InputAction.CallbackContext context)
+    private Vector2 MovePlayer()
     {
-        Vector2 direction = context.ReadValue<Vector2>();
+        Vector2 direction = moveAction.ReadValue<Vector2>();
 
-        transform.Translate(0 , 0, direction.y * Time.deltaTime * movementSpeed);
-        Vector2 rotationInput = context.ReadValue<Vector2>();
+        transform.Translate(0, 0, direction.y * Time.deltaTime * movementSpeed);
+
+        return direction;
+    }
+
+    private void RotatePlayer()
+    {
+        Vector2 rotationInput = moveAction.ReadValue<Vector2>();
         float rotationAmount = rotationInput.x * rotationSpeed * Time.deltaTime;
         transform.Rotate(0f, rotationAmount, 0f);
     }
 
-
     public void Animation()
     {
-       // anim.SetBool("isWalking", Input.GetKey(KeyCode.W));
+        //Empty
     }
 }
