73c8523cf8ccf644898120f3a093fa3b104fc3d7
BugKillR <148430215+BugKillR@users.noreply.github.com>
2024-05-22 19:49:02 +0300

    Commit 46
    Basement Level
    -Market NPC(Cube)
    -Market Script(Canvas)

---
+++ Assets/Scripts/Market.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Market : MonoBehaviour
+{
+    public GameObject marketText, marketCanvas;
+
+    private bool canOpen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canOpen && Input.GetKeyDown(KeyCode.E))
+        {
+            marketCanvas.SetActive(true);
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            marketText.SetActive(true);
+            
+            canOpen = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            marketText.SetActive(false);
+
+            canOpen = false;
+        }
+    }
+
+    public void CloseMarketButton()
+    {
+        marketCanvas.SetActive(false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1;
+    }
+}
